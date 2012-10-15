ab7d0116c773dacac58ea172d9399985f8fd9bf5
Jan Sochiera <jan.sochiera@gmail.com>
2012-10-15 22:45:13 +0200

    zobaczymy co z tego wyjdzie

---
+++ app/controllers/product_controller.rb
@@ -0,0 +1,5 @@
+class ProductController < ActionController::Base
+  def show
+  	
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,8 @@ SklepNaSzynach::Application.routes.draw do
   # just remember to delete public/index.html.
   root :to => 'shop#index'
 
+  resources :products
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
