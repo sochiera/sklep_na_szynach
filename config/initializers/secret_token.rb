f14d53faf6e02d385d409c1877a30518e8fcbea9
Jan Sochiera <jan.sochiera@gmail.com>
2012-10-15 21:07:57 +0200

    na nowo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SklepNaSzynach::Application.config.secret_token = '1e4f6e2a3385c38db248f1e0de4d5ae3528475d35415db191170f2d4575a516c79cd1ced462553cf9066ef210b191a64e25c763eea0d355c5696e27c23881a65'
